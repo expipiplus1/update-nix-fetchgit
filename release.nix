c820f7bfad87ba9dc54fdcb61ad0ca19ce355c94
Joe Hermaszewski <git@monoid.al>
2017-05-04 10:52:46 +0100

    Raise bound on aeson and remove hnix override
    The override is no longer necessary.
    
    Closes #13

--- release.nix
+++ release.nix
@@ -3,24 +3,7 @@
 
 let
   # The last commit of nixpkgs known to work when imported here is
-  # c66ca712b9c4af60477b7582f73a14ab975ffda7 (2016-08-11).
+  # a9f054c8348bf8cd7630cd99def2fd86d22c78bd (2017-05-04).
   pkgs = import <nixpkgs> { };
-
-  hnixSrc = pkgs.fetchFromGitHub {
-    owner = "jwiegley";
-    repo = "hnix";
-    rev = "e2b80391bb731c80995a3a7f2dc0df6685c643c6";
-    sha256 = "0wxv5gmq7w3j8rzs000hskmbvdizcrhf44vpjw2xja519a4fz2r8";
-  };
-
-  # TODO: Use pkgs.haskellPackages after we fix the build on GHC 8.
-  # We would need to fix https://github.com/jwiegley/hnix/issues/39
-  baseHaskellPackages = pkgs.haskell.packages.ghc7103;
-
-  haskellPackages = baseHaskellPackages.override {
-    overrides = self: super: {
-      hnix = self.callPackage (hnixSrc + "/project.nix") { };
-    };
-  };
 in
-  haskellPackages.callPackage src {}
+  pkgs.haskell.packages.ghc802.callPackage src {}
