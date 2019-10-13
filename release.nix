2895fecb4ddd214ef67b986b0e82ceaf748a9f3e
Joe Hermaszewski <git@monoid.al>
2019-10-13 13:48:29 +0800

    Make nix expression better

--- release.nix
+++
@@ -1,9 +0,0 @@
-{ src ? { outPath = ./.; revCount = 0; gitTag = "dirty"; }
-}:
-
-let
-  # The last commit of nixpkgs known to work when imported here is
-  # a9f054c8348bf8cd7630cd99def2fd86d22c78bd (2017-05-04).
-  pkgs = import <nixpkgs> { };
-in
-  pkgs.haskell.packages.ghc802.callPackage src {}
--- shell.nix
+++ shell.nix
@@ -1,1 +1,3 @@
-(import ./release.nix {}).env
+{ pkgs ? import <nixpkgs> { }, compiler ? null, hoogle ? true }:
+
+(import ./default.nix { inherit pkgs compiler hoogle; }).env
