ab5f9f725ce3d308beb83d192dd37bc96db6e30b
Joe Hermaszewski <git@monoid.al>
2020-11-07 15:12:05 +0800

    Bump nixpkgs version

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
     url =
-      "https://github.com/NixOS/nixpkgs/archive/50a8d606f3527b88fbd9f68048944470c1b900cd.tar.gz"; # haskell-updates
-    sha256 = "07xlglmlhf3q6h8cdi3rfg4b0jk70h42kwpgwdh5b1vz149njins";
+      "https://github.com/NixOS/nixpkgs/archive/3561ed6953f96e2178142b04413d200be94e281d.tar.gz"; # master
+    sha256 = "1lsg2dw4ifilywis3638023h4c8b073k2v56qzqqkhl7jkaiphbk";
   };
 
 in import nixpkgsSrc { }
