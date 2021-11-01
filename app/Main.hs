6e3a7ecde62c18ba83ec0a48b8a1a76a9bfff269
Ellie Hermaszewska <git@monoid.al>
2021-11-01 17:05:01 +0800

    formatting

--- app/Main.hs
+++ app/Main.hs
@@ -64,7 +64,8 @@ data Options w = Options
   , attribute
       :: w ::: [Regex] <?> "Pattern (POSIX regex) to limit updates to expressions under matching names in attrsets and let bindings. Combined using inclusive or, if this isn't specified then no expressions will be filtered by attribute name"
   , dryRun :: w ::: Bool <!> "False" <?> "Don't modify the file"
-  , onlyCommented :: w ::: Bool <!> "False" <?> "Only update from Git sources which have a comment on the 'rev' (or 'url' for fetchTarball from GitHub) attribute"
+  , onlyCommented
+      :: w ::: Bool <!> "False" <?> "Only update from Git sources which have a comment on the 'rev' (or 'url' for fetchTarball from GitHub) attribute"
   }
   deriving stock Generic
 
