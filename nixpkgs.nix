becdcf38ad88f9e395f0c0e3bda35720bce05611
Ellie Hermaszewska <git@monoid.al>
2022-04-07 10:32:23 +0800

    nixpkgs bump

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,8 +1,8 @@
 let
   nixpkgsSrc = builtins.fetchTarball {
     url =
-      "https://github.com/NixOS/nixpkgs/archive/16f3fbbd6f332daeb36c1c3fa75587ff6d6faf5a.tar.gz"; # haskell-updates
-    sha256 = "1c1f0ppqidffbgnqc008ljazqmb3wisd92g6w4xq31bq98payxnf";
+      "https://github.com/NixOS/nixpkgs/archive/3bde7576132241b83f37d53d09bac5ba8458a1ee.tar.gz"; # haskell-updates
+    sha256 = "0r83g6wc18cz5fp8dqw836r5aa4jdwlnd9ka4q44q0xzm5mlzw8z";
   };
 
 in import nixpkgsSrc { }
